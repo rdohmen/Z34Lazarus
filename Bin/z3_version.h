6864f490441c3f57d234b494217d0d150a18b3d1
René Dohmen <r_dohmen@hotmail.com>
2021-03-30 12:14:00 +0200

    Add files via upload

---
+++ Bin/z3_version.h
@@ -0,0 +1,7 @@
+// automatically generated file.
+#define Z3_MAJOR_VERSION   4
+#define Z3_MINOR_VERSION   8
+#define Z3_BUILD_NUMBER    5
+#define Z3_REVISION_NUMBER 10519
+
+#define Z3_FULL_VERSION    "Z3 4.8.5.10519 7fb2c6a908d5 master z3-4.8.3-435-g7fb2c6a90"
